efd4ce1995e5c424bdb97ac25fd8e7d19be3a72b
11ze <wangzecn@qq.com>
2024-06-04 20:25:27 +0800

    fix:

--- src/bilibili-live-mask.js
+++ src/bilibili-live-mask.js
@@ -1,28 +1,17 @@
 // ==UserScript==
 // @name         哔哩哔哩移除直播遮挡区域
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0
+// @version      0.1.1
 // @description  无
 // @author       11ze
 // @match        https://live.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
-// @grant        none
+// @grant        GM_addStyle
 // @license      MIT
 // ==/UserScript==
 
 (function () {
   'use strict';
 
-  function main() {
-    const mask = document.querySelector('#web-player-module-area-mask-panel > div');
-
-    if (mask) {
-      mask.style.width = '0%';
-      mask.style.height = '0%';
-    }
-  }
-
-  setInterval(function () {
-    main();
-  }, 2000);
+  GM_addStyle('.web-player-module-area-mask { width: 0 !important; height: 0 !important; }');
 })();
