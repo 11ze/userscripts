263ad22234f1056babeefed602fb79269ecbd94d
11ze <wangzecn@qq.com>
2026-01-27 19:23:46 +0800

    fix: 更新 icon

--- src/anime_search.user.js
+++ src/anime_search.user.js
@@ -5,7 +5,7 @@
 // @description  2026-01-27
 // @author       11ze
 // @match        *://*/*
-// @icon         https://www.agedm.io/favicon.ico
+// @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJmZWF0aGVyIGZlYXRoZXItc2VhcmNoIj48Y2lyY2xlIGN4PSIxMSIgY3k9IjExIiByPSI4Ij48L2NpcmNsZT48cGF0aCBkPSJtMjEgMjEtNC4zNS00LjM1Ij48L3BhdGg+PC9zdmc+
 // @grant        GM_addStyle
 // @license      MIT
 // ==/UserScript==
